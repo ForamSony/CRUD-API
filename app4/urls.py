a01db4648c3d2be9e3e30c30666c482918a0b1c8
foram <foramsony29@gmail.com>
2024-03-04 17:06:20 +0530

    token authentication comitted by foram

--- app4/urls.py
+++ app4/urls.py
@@ -1,13 +1,17 @@
 from django.urls import path,include
 from app4 import views
 from rest_framework.routers import DefaultRouter
+from rest_framework.authtoken.views import obtain_auth_token
+from app4.auth import customauthtoken
+
 
 router = DefaultRouter()
 router.register('stdapi',views.StudentModelViewSet,basename='student')
 
 urlpatterns = [
    path('', include(router.urls)),
-   path('auth/',include('rest_framework.urls', namespace='rest_framework'))
-
+   path('auth/',include('rest_framework.urls', namespace='rest_framework')),
+   # path('gettoken/', obtain_auth_token),
+   path('gettoken/', customauthtoken.as_view()),
 ]
 
--- app4/views.py
+++ app4/views.py
@@ -1,7 +1,7 @@
 from .models import Student
 from .serializer import studentserializer
 from rest_framework import viewsets
-from rest_framework.authentication import BasicAuthentication,SessionAuthentication
+from rest_framework.authentication import BasicAuthentication,SessionAuthentication,TokenAuthentication
 from rest_framework.permissions import IsAuthenticated,AllowAny,IsAdminUser,IsAuthenticatedOrReadOnly,DjangoModelPermissions,DjangoModelPermissionsOrAnonReadOnly
 from .custompermission import  MyPermission
 
@@ -10,11 +10,13 @@ class StudentModelViewSet(viewsets.ModelViewSet):
     queryset = Student.objects.all()
     serializer_class = studentserializer
     # authentication_classes = [BasicAuthentication]
-    authentication_classes = [SessionAuthentication]
+ #   authentication_classes = [SessionAuthentication]
     # permission_classes = [IsAuthenticated]
     # permission_classes = [IsAdminUser]
     #permission_classes = [AllowAny]
     #permission_classes = [IsAuthenticatedOrReadOnly]
     #permission_classes=[DjangoModelPermissions]
     # permission_classes = [DjangoModelPermissionsOrAnonReadOnly]
-    permission_classes = [MyPermission]
+#    permission_classes = [MyPermission]
+
+
