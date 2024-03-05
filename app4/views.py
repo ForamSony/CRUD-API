1ebbee9e6c77d96a6445756ecdba3d848b7fb56f
foram <foramsony29@gmail.com>
2024-03-05 12:04:39 +0530

    Custom Authentication comitted by foram

--- app4/views.py
+++ app4/views.py
@@ -4,14 +4,16 @@ from rest_framework import viewsets
 from rest_framework.authentication import BasicAuthentication,SessionAuthentication,TokenAuthentication
 from rest_framework.permissions import IsAuthenticated,AllowAny,IsAdminUser,IsAuthenticatedOrReadOnly,DjangoModelPermissions,DjangoModelPermissionsOrAnonReadOnly
 from .custompermission import  MyPermission
-
+from .customAuth import CustomAuthentication
 class StudentModelViewSet(viewsets.ModelViewSet):
 
-    queryset = Student.objects.all()
-    serializer_class = studentserializer
+     queryset = Student.objects.all()
+     serializer_class = studentserializer
     # authentication_classes = [BasicAuthentication]
  #   authentication_classes = [SessionAuthentication]
-    # permission_classes = [IsAuthenticated]
+     #authentication_classes = [TokenAuthentication]
+     authentication_classes = [CustomAuthentication]
+     permission_classes = [IsAuthenticated]
     # permission_classes = [IsAdminUser]
     #permission_classes = [AllowAny]
     #permission_classes = [IsAuthenticatedOrReadOnly]
