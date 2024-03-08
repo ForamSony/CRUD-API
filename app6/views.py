f8ee66b669439c8af28f9a6f770c7d5b229954ed
foram <foramsony29@gmail.com>
2024-03-08 15:26:04 +0530

    Ordering filter comitted by foram

--- app6/views.py
+++ app6/views.py
@@ -3,7 +3,7 @@ from .serializers import StudentSerializer
 from rest_framework.generics import ListAPIView
 from .models import Student
 from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework.filters import SearchFilter
+from rest_framework.filters import SearchFilter,OrderingFilter
 # Create your views here.
 
 class StudentList(ListAPIView):
@@ -15,14 +15,20 @@ class StudentList(ListAPIView):
     # #filterset_fields = ['city']
     # filterset_fields = ['name','city']
 
-
-    filter_backends = [SearchFilter]
-    search_fields = ['city']
+#search filter==========
+    # filter_backends = [SearchFilter]
+    # search_fields = ['city']
     # search_fields = ['name', 'city']
     # search_fields = ['^name'] # start with
     #search_fields = ['=name'] # exact match
     # search_fields = ['@name'] # full text search
     # search_fields = ['$name'] # regex search
+
+#ordering filter===========
+    filter_backends = [OrderingFilter]
+    # ordering_fields =['name']
+    ordering_fields =['name','city']
+
     # def get_queryset(self):
     #     user = self.request.user
     #     return Student.objects.filter(passby=user)
