834c4acf5e7355a23958bd0ee66f424b9c4217fe
foram <foramsony29@gmail.com>
2024-03-08 16:42:47 +0530

    Limit Offset pagination filter comitted by foram

--- app6/mypagination.py
+++ app6/mypagination.py
@@ -1,8 +1,14 @@
-from rest_framework.pagination import PageNumberPagination
+from rest_framework.pagination import PageNumberPagination,LimitOffsetPagination
 
-class My(PageNumberPagination):
-    page_size = 5 # 5 record in 1 page
-    page_query_param = 'p'
-    page_size_query_param = 'recordes' #userchoice howmuch records in singlepage
-    max_page_size = 6 # maxi records in singlepage
-    last_page_strings = 'end'
+# class My(PageNumberPagination):
+#     page_size = 5 # 5 record in 1 page
+#     page_query_param = 'p'
+#     page_size_query_param = 'recordes' #userchoice howmuch records in singlepage
+#     max_page_size = 6 # maxi records in singlepage
+#     last_page_strings = 'end'
+
+class Mylimit(LimitOffsetPagination):
+   default_limit = 5
+   # limit_query_param = 'l'
+   # offset_query_param = 'o'
+   max_limit = 6
--- app6/views.py
+++ app6/views.py
@@ -3,12 +3,14 @@ from .serializers import StudentSerializer
 from rest_framework.generics import ListAPIView
 from .models import Student
 from .serializers import StudentSerializer
-from .mypagination import My
+from .mypagination import Mylimit
+#from .mypagination import My
 # Create your views here.
 
 class StudentList(ListAPIView):
     queryset = Student.objects.all()
     serializer_class = StudentSerializer
-    pagination_class = My
+    #pagination_class = My
+    pagination_class = Mylimit
 
 
